a651a00f5691c19b74d5be08e638ece2ebdc6d17
MuffinOwl <semenulse@gmail.com>
2022-03-23 03:39:04 +0700

    fixed

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,7 @@
 const comparison = document.querySelector('.comparison');
 
-const imageBefore = comparison.querySelector('.comparison__image--before');
-const imageAfter = comparison.querySelector('.comparison__image--after');
+const innerBefore = comparison.querySelector('.comparison__inner--before');
+const innerAfter = comparison.querySelector('.comparison__inner--after');
 
 const buttonBefore = comparison.querySelector('.comparison__button--before');
 const buttonAfter = comparison.querySelector('.comparison__button--after');
@@ -9,18 +9,18 @@ const sliderScale = comparison.querySelector('.comparison__slider-scale');
 const sliderBar = comparison.querySelector('.comparison__slider-bar');
 
 buttonBefore.addEventListener('click', function() {
-  imageBefore.style.width = '100%';
-  imageAfter.style.width = '0';
+  innerBefore.style.width = '100%';
+  innerAfter.style.width = '0';
   sliderBar.value = 0;
 })
 
 buttonAfter.addEventListener('click', function() {
-  imageBefore.style.width = '0';
-  imageAfter.style.width = '100%';
+  innerBefore.style.width = '0';
+  innerAfter.style.width = '100%';
   sliderBar.value = 100;
 })
 
 sliderBar.addEventListener('input', function() {
-  imageBefore.style.width = (100 - sliderBar.value) + '%';
-  imageAfter.style.width = sliderBar.value + '%';
+  innerBefore.style.width = (100 - sliderBar.value) + '%';
+  innerAfter.style.width = sliderBar.value + '%';
 })
